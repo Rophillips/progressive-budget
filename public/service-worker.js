c3b902996569d3a2f395a7ea3bcb11edd7a435eb
Roseanne Phillips <rphillips.rp@gmail.com>
2021-04-19 22:01:39 -0400

    registered service worker

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/app.js",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+
+] 
